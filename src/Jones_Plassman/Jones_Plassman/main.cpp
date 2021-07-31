02b5c5a9704fc3583b64d144f544f8a561aa1e42
GIOELE SCALETTA s284100 <s284100@studenti.polito.it>
2021-08-01 01:25:31 +0200

    fix-logic-hyperparam-not-yet-complete-csv

--- src/Jones_Plassman/Jones_Plassman/main.cpp
+++ src/Jones_Plassman/Jones_Plassman/main.cpp
@@ -2,28 +2,46 @@
 
 #include "graph.h"
 #include <iostream>
-#include <fstream>  
+#include <fstream>
 #include <iomanip>
-#include <experimental/filesystem> 
+#include <experimental/filesystem>
+#include <map>
 namespace fs = std::experimental::filesystem;
 
+
+
 int main() {
-	
+
 	clock_t start, end;
 	double time_not_parallel;
 	string name;
 	int color_parallel;
 	double time, score;
-	double min_time=0, curr_time;
-	int best_coef, best_n_thread;
+	map<int, double> graphs_best_parameters;
+	double /*min_time=0,*/ curr_time;
+	//int best_coef, best_n_thread;
+	double min = 0;
+	int mini, minj;
 	string graph_sample, graph_sample_path;
+	vector<int> N_threads = { 2, 3, 5, 7, 8, 15 }; //!!!!!Here probably better to use numbers rwelative to max_threads and thus relative to machine on which program running so that program sort of fine tunes itself deopending on pc features
+	vector<int> coefs = { 1, 5, 10, 100 };
+	int coef, n_threads;
+	int output_width = 50, i, j;
 
-	int output_width = 50;
+	//sistema con matrice in cui per ogni n_thread e coef value salviamo la somma dei tempi per poi trovare la combinazione di parametri con somma minore. Un modo
+	// "più pulito" sarebbe semplicemente spostare il for più esterno (itera sui grafi) come più interno, ma questo comporterebbe ricaricare ogni volta i grafi
+
+	double best_res_mat[/*N_threads.size()*/6][/*coefs.size()*/ 4];
+	for (i = 0; i < N_threads.size(); i++)
+		for (j = 0; j < coefs.size(); j++)
+			best_res_mat[i][j] = 0;
 
 	ofstream myfile;
 	myfile.open("../../../results.csv", std::ofstream::trunc);
-		
+
+
 	for (auto& p : fs::recursive_directory_iterator("../../../../benchmarks/benchmarks/rgg")) {
+
 
 		if (p.path().string().compare(p.path().string().size() - 4, 4, ".gra") == 0 || p.path().string().compare(p.path().string().size() - 6, 6, ".graph") == 0) {
 			graph myGraph = graph();
@@ -39,13 +57,17 @@ int main() {
 			else
 				myGraph.readFileDIMACS(graph_sample_path);
 
-
 			cout << "Graph: " << graph_sample << endl;
 
-			for (int n_threads : {2, 3, 5, 8, 12, 20}) {
-				for (int coef : { 1, 5, 10, 100 }) {
+			for (i = 0; i < N_threads.size(); i++) {
+				n_threads = N_threads[i];
 
-					curr_time = 0;
+				for (j = 0; j < coefs.size(); j++) {
+					coef = coefs[j];
+
+					curr_time = best_res_mat[i][j];
+
+
 					/*Largest Degree First Standard*/
 					start = clock();
 					myGraph.LargestDegreeFirstStandard(n_threads, coef);
@@ -66,7 +88,7 @@ int main() {
 
 					myGraph.cancelColors();
 
-					
+
 					/*Smallest Degree Last*/
 					start = clock();
 					myGraph.SmallestDegreeLastStandard(n_threads, coef);
@@ -110,30 +132,51 @@ int main() {
 					else
 						cout << "Coloring is wrong!" << endl;
 
-					if (curr_time < min_time) {
-						min_time = curr_time;
-						best_coef = coef;
-						best_n_thread = n_threads;
-					}
-
 					myGraph.cancelColors();
+
+					best_res_mat[i][j] = curr_time;
+
 				}
+
+				/*if (curr_time < min_time) {
+				min_time = curr_time;
+				best_coef = coef;
+				best_n_thread = n_threads;
+				}*/
 			}
 		}
-		
+
 	}
 
+	//find best hyperparameters
+	for (i = 0; i < N_threads.size(); i++)
+		for (j = 0; j < coefs.size(); j++)
+			if (min > best_res_mat[i][j]) {
+				min = best_res_mat[i][j];
+				mini = i;
+				minj = j;
+			}
+
+	n_threads = N_threads[mini];
+	coef = coefs[minj];
+
+	myfile << "best results: n_thread= " << n_threads << " coef= " << coef << endl;
+
+	cout << "best results: n_thread= " << n_threads << " coef= " << coef << endl;
+
 	myfile << "Run performed on DELL XPS" << endl;
+
 	myfile.close();
-	
+
 	myfile.open("../../../results_best.csv", std::ofstream::trunc);
 
 
 	/*
-	 * Run with best n_threads and coef
-	 */
-	int n_threads = best_n_thread;
-	int coef = best_coef;
+	* Run with best n_threads and coef
+	*/
+	//int n_threads = best_n_thread;
+	//int coef = best_coef;
+
 
 	for (auto& p : fs::recursive_directory_iterator("../../../../benchmarks/benchmarks")) {
 
@@ -252,7 +295,7 @@ int main() {
 
 			myGraph.cancelColors();
 
-				/*Jones Plassman sequential*/
+			/*Jones Plassman sequential*/
 
 			start = clock();
 			myGraph.JonesPlassmanColoringSequential();
@@ -271,8 +314,8 @@ int main() {
 			else
 				cout << "Coloring is wrong!" << endl;
 
-				myGraph.cancelColors();
-	
+			myGraph.cancelColors();
+
 			/*Jones Plassman standard with threadpool*/
 
 			start = clock();
@@ -367,50 +410,50 @@ int main() {
 				/*Smallest Degree Last sequential*/
 
 
-/*Smallest Degree Last parallel weighing*/
-			/*start = clock();
-			myGraph.SmallestDegreeLastParallelWeighing();
-			end = clock();
+				/*Smallest Degree Last parallel weighing*/
+							/*start = clock();
+							myGraph.SmallestDegreeLastParallelWeighing();
+							end = clock();
 
-			color_parallel = myGraph.checkColoringCSR();
-
-
-			name = "Smallest Degree Last (parallel weighing)";
-			cout << setw(output_width) << name << " :";
-
-			if (color_parallel != -1) {
-				time = double(end - start) / double(CLOCKS_PER_SEC);
-				cout << "Time (sec): " << time << " \tColors: " << color_parallel << endl;
-				saveToFile(myfile, graph_sample, name, time, color_parallel);
-			}
-			else
-				cout << "Coloring is wrong!" << endl;
-
-			myGraph.cancelColors();
+							color_parallel = myGraph.checkColoringCSR();
 
 
-/*map<string, string> GRAPHS = {{"citeseer_sub_10720.gra", "../../../../benchmark/small_dense_real/citeseer_sub_10720.gra"}, {"ba10k5d", "../../../../benchmark/scaleFree/ba10k5d.gra"},  {"mtbrv_dag_uniq.gra" , "../../../../benchmark/sigmod08/mtbrv_dag_uniq.gra"},
-	{"v100.gra", "../../../../benchmark/manual/v100.gra"}, {"ba10k2d", "../../../../benchmark/scaleFree/ba10k2d.gra"},  {"agrocyc_dag_uniq.gra" , "../../../../benchmark/sigmod08/agrocyc_dag_uniq.gra"},
-	{"anthra_dag_uniq.gra" , "../../../../benchmark/sigmod08/anthra_dag_uniq.gra"}, {"ecoo_dag_uniq.gra" , "../../../../benchmark/sigmod08/ecoo_dag_uniq.gra"}
-	,{"citeseer.scc.gra", "../../../../benchmark/large/citeseer.scc.gra"}
-	};*/
+							name = "Smallest Degree Last (parallel weighing)";
+							cout << setw(output_width) << name << " :";
 
-	/*map<string, string> GRAPHS = { {"uniprotenc_22m", "../../../../benchmark/large/uniprotenc_22m.scc.gra"},
-	{"uniprotenc_100m", "../../../../benchmark/large/uniprotenc_100m.scc.gra"}};
+							if (color_parallel != -1) {
+								time = double(end - start) / double(CLOCKS_PER_SEC);
+								cout << "Time (sec): " << time << " \tColors: " << color_parallel << endl;
+								saveToFile(myfile, graph_sample, name, time, color_parallel);
+							}
+							else
+								cout << "Coloring is wrong!" << endl;
 
-	//PER PC GIO
-map<string, string> GRAPHS = { /*{ "ba10k5d", "../../../../benchmarks/benchmarks/benchmark/scaleFree/ba10k5d.gra"} , {"uniprotenc_100m", "../../../../benchmarks/benchmarks/benchmark/large/uniprotenc_100m.scc.gra"} , {"uniprotenc_22m", "../../../../benchmarks/benchmarks/benchmark/large/uniprotenc_22m.scc.gra"},
-{"citeseer_sub_10720.gra", "../../../../benchmarks/benchmarks/benchmark/small_dense_real/citeseer_sub_10720.gra"},  {"mtbrv_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/mtbrv_dag_uniq.gra"},
-{"v100.gra", "../../../../benchmarks/benchmarks/benchmark/manual/v100.gra"}, {"ba10k2d", "../../../../benchmarks/benchmarks/benchmark/scaleFree/ba10k2d.gra"},  {"agrocyc_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/agrocyc_dag_uniq.gra"},
-{"anthra_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/anthra_dag_uniq.gra"}, {"ecoo_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/ecoo_dag_uniq.gra"}
-, {"citeseer.scc.gra", "../../../../benchmarks/benchmarks/benchmark/large/citeseer.scc.gra"} };
-{"citeseerx", "../../../../benchmarks/benchmarks/benchmark/large/citeseerx.gra"} */
-	  
+							myGraph.cancelColors();
 
-// This does not work :( -> or maybe it works but it takes very very long
-//map<string, string> GRAPHS = { {"citeseerx", "../../../../benchmark/large/citeseerx.gra"} };
 
-//map<string, string> GRAPHS = { {"go_uniprot", "../../../../benchmark/large/go_uniprot.gra"} };
-//map<string, string> GRAPHS = { { "ba10k5d", "../../../../benchmarks/benchmarks/benchmark/scaleFree/ba10k5d.gra"} };
+				/*map<string, string> GRAPHS = {{"citeseer_sub_10720.gra", "../../../../benchmark/small_dense_real/citeseer_sub_10720.gra"}, {"ba10k5d", "../../../../benchmark/scaleFree/ba10k5d.gra"},  {"mtbrv_dag_uniq.gra" , "../../../../benchmark/sigmod08/mtbrv_dag_uniq.gra"},
+					{"v100.gra", "../../../../benchmark/manual/v100.gra"}, {"ba10k2d", "../../../../benchmark/scaleFree/ba10k2d.gra"},  {"agrocyc_dag_uniq.gra" , "../../../../benchmark/sigmod08/agrocyc_dag_uniq.gra"},
+					{"anthra_dag_uniq.gra" , "../../../../benchmark/sigmod08/anthra_dag_uniq.gra"}, {"ecoo_dag_uniq.gra" , "../../../../benchmark/sigmod08/ecoo_dag_uniq.gra"}
+					,{"citeseer.scc.gra", "../../../../benchmark/large/citeseer.scc.gra"}
+					};*/
 
-//map<string, string> GRAPHS = { {"citeseer.scc", "../../../../benchmark/large/citeseer.scc.gra"} };
+					/*map<string, string> GRAPHS = { {"uniprotenc_22m", "../../../../benchmark/large/uniprotenc_22m.scc.gra"},
+					{"uniprotenc_100m", "../../../../benchmark/large/uniprotenc_100m.scc.gra"}};
+
+					//PER PC GIO
+				map<string, string> GRAPHS = { /*{ "ba10k5d", "../../../../benchmarks/benchmarks/benchmark/scaleFree/ba10k5d.gra"} , {"uniprotenc_100m", "../../../../benchmarks/benchmarks/benchmark/large/uniprotenc_100m.scc.gra"} , {"uniprotenc_22m", "../../../../benchmarks/benchmarks/benchmark/large/uniprotenc_22m.scc.gra"},
+				{"citeseer_sub_10720.gra", "../../../../benchmarks/benchmarks/benchmark/small_dense_real/citeseer_sub_10720.gra"},  {"mtbrv_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/mtbrv_dag_uniq.gra"},
+				{"v100.gra", "../../../../benchmarks/benchmarks/benchmark/manual/v100.gra"}, {"ba10k2d", "../../../../benchmarks/benchmarks/benchmark/scaleFree/ba10k2d.gra"},  {"agrocyc_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/agrocyc_dag_uniq.gra"},
+				{"anthra_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/anthra_dag_uniq.gra"}, {"ecoo_dag_uniq.gra" , "../../../../benchmarks/benchmarks/benchmark/sigmod08/ecoo_dag_uniq.gra"}
+				, {"citeseer.scc.gra", "../../../../benchmarks/benchmarks/benchmark/large/citeseer.scc.gra"} };
+				{"citeseerx", "../../../../benchmarks/benchmarks/benchmark/large/citeseerx.gra"} */
+
+
+				// This does not work :( -> or maybe it works but it takes very very long
+				//map<string, string> GRAPHS = { {"citeseerx", "../../../../benchmark/large/citeseerx.gra"} };
+
+				//map<string, string> GRAPHS = { {"go_uniprot", "../../../../benchmark/large/go_uniprot.gra"} };
+				//map<string, string> GRAPHS = { { "ba10k5d", "../../../../benchmarks/benchmarks/benchmark/scaleFree/ba10k5d.gra"} };
+
+				//map<string, string> GRAPHS = { {"citeseer.scc", "../../../../benchmark/large/citeseer.scc.gra"} };
